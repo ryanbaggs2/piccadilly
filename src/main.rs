cbcbd23674e07ec61a6aad9eeb30bbfeec9e9a2c
Ryan Baggs <ryanbaggs7@gmail.com>
2022-12-30 15:04:18 -0800

    Add project plan. Add winit crate.
    Add winit crate to create a Window for the program.
    
    Signed-off-by: Ryan Baggs <ryanbaggs7@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
 fn main() {
-    println!("Hello, world!");
+    // These are the project details.
+    // Bus/train ticket system (Rust/winit)
+    //
+    // - Create a random Ticket ID
+    // - Get the users name
+    // - Get the users credit card info
+    // - Ask to save information for future
+    // - Provide clickable map to select destination
+    // - Find the quickest route to a destination through multiple algorithms
+    // - Provide selections for specific algorithm to use
+    // - Save the records to an encrypted database
+    // - Retrieve records from specific dates/times
+    //
+    // - Utilize hash table for the unique ticket information
+    // - Graph algorithm for routes
 }
